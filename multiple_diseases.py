57a4db353f5bd1b195c9663170fe3c7ae4b85de5
winfry77 <Winfrynyarangi@gmail.com>
2023-08-14 00:46:52 +0300

    PredictionAndRecommendationSystemKenyanHealth

--- multiple_diseases.py
+++ multiple_diseases.py
@@ -6,3 +6,21 @@ from streamlit_option_menu import option_menu
 
 kidney_model = pickle.load(open('kidney_model.sav', 'rb'))
 
+#SIDEBAR FOR NAVIGATION
+with st.sidebar:
+    selected = option_menu('Multiple Disease Prediction System',
+                           
+                           ['Chronic Kidney Prediction',
+                            'Cancer Prediction',
+                            'Heart Prediction ',
+                            'Diabetes Prediction'],
+
+                            icons=['activity','heart','person'],
+                            default_index=0)
+
+
+#CHRONIC KIDNEY PREDICTION PAGE 
+if (selected == 'Chronic Kidney Prediction'): 
+
+    #Page Title
+    st.title('Chronic Kidney Disease Prediction Using Machine Learning')             
